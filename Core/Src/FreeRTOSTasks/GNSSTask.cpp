9fbba32adb1a8976db6d961d9fa14eb56d60826c
sourland <jem2010@windowslive.com>
2024-01-29 09:50:29 +0200

    configure gnss task to accept messages

--- Core/Src/FreeRTOSTasks/GNSSTask.cpp
+++ Core/Src/FreeRTOSTasks/GNSSTask.cpp
@@ -4,33 +4,15 @@
 
 void GNSSTask::execute() {
     taskHandle = xTaskGetCurrentTaskHandle();
-
     HAL_GPIO_WritePin(P5V_RF_EN_GPIO_Port, P5V_RF_EN_Pin, GPIO_PIN_SET);
-    auto baudrate = GNSSDefinitions::BaudRate::Option115200;
-    GNSSDefinitions::SoftwareType softwareType = GNSSDefinitions::SoftwareType::Reserved;
-//    const uint8_t *message = GNSSReceiver::querySoftwareVersion(softwareType);
-//    message = GNSSReceiver::setFactoryDefaults();
-      uint8_t message[] = {0xA0, 0xA1, 0x00, 0x02, 0x02, 0x00, 0x02, 0x0D, 0x0A};
-
-
     while(true){
-        auto volatile status = HAL_UART_Transmit(&huart5, message, 9, 1000);
+        xTaskNotifyWait(0, 0, nullptr, portMAX_DELAY);
 
-//        xTaskNotifyWait(0, 0, nullptr, portMAX_DELAY);
-
-        uint8_t omg[]  = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-//        HAL_UART_Transmit(&huart5, message.messageBody.data(), 10, 1000);
-//        HAL_Delay(10000);
-        status = HAL_UART_Receive(&huart5, incomingMessage, 512, 1000);
-        status = HAL_UART_Receive(&huart5, incomingMessage, 512, 1000);
-        etl::string<1000> minima = {};
+        etl::string<512> GNSSMessage = {};
         for (uint8_t  byte : incomingMessage) {
-            minima.push_back(byte);
+            GNSSMessage.push_back(byte);
         }
-        LOG_DEBUG << minima.c_str();
-//        MessageParser::execute(ecssTC);
-
-//        incomingMessage.clear();
+        LOG_DEBUG << GNSSMessage.c_str();
 
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
