9ca9e5da72784ab242ec493e55c161434acc56ec
andronikos <andronikostaslos@gmail.com>
2024-07-22 18:20:27 +0300

    add a delay for the transceiver task

--- Core/Src/FreeRTOSTasks/TransceiverTask.cpp
+++ Core/Src/FreeRTOSTasks/TransceiverTask.cpp
@@ -129,6 +129,7 @@ void TransceiverTask::modulationConfig(){
 
 void TransceiverTask::execute(){
     // ENABLE THE RF 5V
+    vTaskDelay(pdMS_TO_TICKS(10000));
     HAL_GPIO_WritePin(P5V_RF_EN_GPIO_Port, P5V_RF_EN_Pin, GPIO_PIN_SET);
     LOG_DEBUG << "RF 5V ENABLED " ;
     vTaskDelay(pdMS_TO_TICKS(1000));
